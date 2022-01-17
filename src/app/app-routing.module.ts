479c4a12725f796c6ddeef8bb9ab64f3af30bac3
Jigaboy10 <jigard.bhanushali12@gmail.com>
2022-01-17 19:50:29 +0530

    feat:format components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FormComponent } from './components/form/form.component'
 
 const routes: Routes = [
   { path: 'footer', component: FooterComponent }
